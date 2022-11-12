90c283363f27c3e3b1807524b8e7795ad28845b5
Brian Swedberg <bwswedberg@gmail.com>
2022-11-12 06:48:03 -0500

    Split adapters into different dev rollup builds

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -3,21 +3,51 @@ import nodeResolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import serve from 'rollup-plugin-serve'
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    file: 'dist/bundle.min.js',
-    format: 'umd',
-    name: 'MapTileAdapter',
-    sourcemap: true
+export default [
+  {
+    input: 'src/adapters/maplibre/index.ts',
+    output: {
+      file: 'dist/maplibre/map-tile-adapter.min.js',
+      format: 'umd',
+      name: 'MapTileAdapter',
+      sourcemap: true
+    },
+    plugins: [
+      tsc(), 
+      nodeResolve(), 
+      commonjs()
+    ],
   },
-  plugins: [
-    tsc(), 
-    nodeResolve(), 
-    commonjs(),
-    serve({
-      contentBase: ['demo', 'dist', 'node_modules'],
-      port: 3000
-    })
-  ],
-}
+  {
+    input: 'src/adapters/leaflet/index.ts',
+    output: {
+      file: 'dist/leaflet/map-tile-adapter.min.js',
+      format: 'umd',
+      name: 'MapTileAdapter',
+      sourcemap: true
+    },
+    plugins: [
+      tsc(), 
+      nodeResolve(), 
+      commonjs()
+    ],
+  },
+  {
+    input: 'src/presets/index.ts',
+    output: {
+      file: 'dist/presets/map-tile-adapter-presets.min.js',
+      format: 'umd',
+      name: 'MapTileAdapterPresets',
+      sourcemap: true
+    },
+    plugins: [
+      tsc(), 
+      nodeResolve(), 
+      commonjs(),
+      serve({
+        contentBase: ['demo', 'dist', 'node_modules'],
+        port: 3000
+      })
+    ],
+  }
+];
